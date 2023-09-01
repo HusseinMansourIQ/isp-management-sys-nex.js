b0618d5e8cf9fb6a7a96e79636d5f088b011ed69
HusseinMansourIQ <70377627+HusseinMansourIQ@users.noreply.github.com>
2023-09-01 18:32:49 +0300

    im  stupid

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import { NextAuthOptions } from "next-auth";
 import githubProvider from 'next-auth/providers/github';
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 export const authOptions={
@@ -10,7 +11,7 @@ export const authOptions={
         clientId : process.env.GITHUB_ID,
         clientSecret : process.env.GITHUB_SECRET
     }),
-    githubProvider({
+    GoogleProvider({
         clientId : process.env.GOOGLE_ID,
         clientSecret : process.env.GOOGLE_SECRET
     })
